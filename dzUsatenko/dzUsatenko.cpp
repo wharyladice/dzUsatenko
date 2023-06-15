b8ae1c4b99aa83a77990e329c65bd2ddc9694a54
Admin <Admin@WIN-5I7J1UL4AE7>
2023-06-15 20:11:37 +0300

    Добавьте файлы проекта.

---
+++ dzUsatenko/dzUsatenko.cpp
@@ -0,0 +1,63 @@
+﻿
+
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Календарь мicяця";
+	cout << endl;
+	int number_of_month;
+	cout << "Введiть ваш номер мiсяця: ";
+	cin >> number_of_month;
+	switch (number_of_month) {
+	case 1:
+		cout << "Ciчень" << endl;
+		break;
+	case 2:
+		cout << "Лютий"<<endl;
+		break;
+	case 3:
+		cout << "Березень" << endl;
+		break;
+	case 4:
+		cout << "Квітень" << endl;
+		break;
+	case 5:
+		cout << "Травень" << endl;
+		break;
+	case 6:
+		cout << "Червень" << endl;
+		break;
+	case 7:
+		cout << "Липень" << endl;
+		break;
+	case 8:
+		cout << "Серпень" << endl;
+		break;
+	case 9:
+		cout << "Вересень" << endl;
+		break;
+	case 10:
+		cout << "Жовтень" << endl;
+		break;
+	case 11:
+		cout << "Листопад" << endl;
+		break;
+	case 12:
+		cout << "Грудень" << endl;
+		break;
+	default:
+		cout << "Невiрний номер";
+		break;
+	
+
+
+	}
+}
+		
+
